65ef6099c516ce109ed8bf1940012f751c1c146d
saiki <otoiawasehakotiradayo@gmail.com>
2017-11-23 19:53:43 +0900

    基本のメソッドを形だけ用意

---
+++ image_storage.go
@@ -0,0 +1,23 @@
+package imageStorage
+
+type imageManagerI interface {
+}
+
+type imageManager struct {
+}
+
+func (im *imageManager) Add() {
+
+}
+
+func (im *imageManager) Update() {
+
+}
+
+func (im *imageManager) Delete() {
+
+}
+
+func (im *imageManager) Get() {
+
+}
