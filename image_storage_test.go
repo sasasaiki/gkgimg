3db96f4cbd0c9fae787e333c9e738a97d7b03b95
saiki <otoiawasehakotiradayo@gmail.com>
2017-11-25 15:09:25 +0900

    オリジナルの名前を渡す方にもテストコード追加

--- image_storage_test.go
+++ image_storage_test.go
@@ -158,3 +158,108 @@ func TestPrintError(t *testing.T) {
 		})
 	}
 }
+
+func TestDirImgStorageSaveWithOriginFileName(t *testing.T) {
+
+	type args struct {
+		file           multipart.File
+		originFilename string
+		fileName       string
+		directory      string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "pngをpngとして保存できる",
+			args: args{
+				file:           openFile(PngPath),
+				originFilename: PngFile,
+				fileName:       "test",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "jpegをjpegとして保存できる",
+			args: args{
+				file:           openFile(JpegPath),
+				originFilename: JpegFile,
+				fileName:       "test",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "textも保存できるらしい",
+			args: args{
+				file:           openFile(TextPath),
+				originFilename: TextPath,
+				fileName:       "test",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "jpegをpngとして保存してみる.エラーは出ない",
+			args: args{
+				file:           openFile(JpegPath),
+				originFilename: PngFile,
+				fileName:       "test_jpg_to",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "pngをjpegとして保存してみる.エラーは出ない",
+			args: args{
+				file:           openFile(PngPath),
+				originFilename: JpegFile,
+				fileName:       "test_png_to",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "textをjpegとして保存してみる.エラーは出ないがひらけないファイルができる",
+			args: args{
+				file:           openFile(TextPath),
+				originFilename: JpegFile,
+				fileName:       "test_text_to",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "jpegをtextとして保存してみる.エラーは出ないがひらけないファイルができる",
+			args: args{
+				file:           openFile(JpegPath),
+				originFilename: TextFile,
+				fileName:       "test_jpeg_to",
+				directory:      "testImageStorage",
+			},
+			wantErr: false,
+		},
+		{
+			name: "閉じられたfileはエラーを返す",
+			args: args{
+				file:           closedFile(JpegPath),
+				originFilename: JpegFile,
+				fileName:       "test_text_to",
+				directory:      "testImageStorage",
+			},
+			wantErr: true,
+		},
+	}
+
+	im := new(DirImgStorage)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := im.SaveWithOriginFileName(tt.args.file, tt.args.originFilename, tt.args.fileName, tt.args.directory); (err != nil) != tt.wantErr {
+				t.Errorf("imageManager.Add() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
